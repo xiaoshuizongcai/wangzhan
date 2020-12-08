7458a19a9826e7b0b7587696b1aa5ca0c97172ab
xiaoshuizongcai <2450752175@qq.com>
2020-12-08 12:19:29 +0800

    wangzhan

--- wangzhan.py
+++ wangzhan.py
@@ -3,6 +3,6 @@ app=Flask(__name__)
 @app.route("/")#制作网站
 def index():
     return render_template('index.html')
-@app.route("/he")
-def he():
-    return render_template('he.html')#在这个网站里进入另一个位置（如翻页、链接）
+@app.route("/he/<name>")
+def he(name):
+    return render_template('he.html',name=name)#在这个网站里进入另一个位置（如翻页、链接）
